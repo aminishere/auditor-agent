64a4951c7640cde0d5b771c4c3a93fdea3a13cd5
aminishere <zainabamin281@gmail.com>
2025-11-01 23:57:56 +0530

    add seo logic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from actions import get_response_time
+from prompts import system_prompt
+from json_helpers import extract_json
 
 # Load environment variables
 load_dotenv()
@@ -19,6 +22,43 @@ def generate_text_with_conversation(messages, model = "meta-llama/Llama-3.1-8B-I
     return response.choices[0].message.content
 
 
-messages = [{"role": "user", "content": "Indian food recipes"}]
+available_actions = {
+    "get_response_time": get_response_time
+}
 
-print(generate_text_with_conversation(messages))
+user_prompt = "What is the response time for google.com?"
+
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": user_prompt},
+]
+
+turn_count = 1
+max_turns = 5
+
+
+while turn_count < max_turns:
+    print (f"Loop: {turn_count}")
+    print("----------------------")
+    turn_count += 1
+
+    response = generate_text_with_conversation(messages, model="meta-llama/Llama-3.1-8B-Instruct")
+
+    print(response)
+
+    json_function = extract_json(response)
+
+    if json_function:
+            function_name = json_function[0]['function_name']
+            function_parms = json_function[0]['function_parms']
+            if function_name not in available_actions:
+                raise Exception(f"Unknown action: {function_name}: {function_parms}")
+            print(f" -- running {function_name} {function_parms}")
+            action_function = available_actions[function_name]
+            #call the function
+            result = action_function(**function_parms)
+            function_result_message = f"Action_Response: {result}"
+            messages.append({"role": "user", "content": function_result_message})
+            print(function_result_message)
+    else:
+         break
